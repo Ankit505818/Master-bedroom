d9aa9e000b5a85562aa3f725dd6fb6d382c58ab5
Ankit505818 <sharmaankit2690@gmail.com>
2025-06-01 15:36:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-adobe-express---file",
+      "name": "Adobe Express - file",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.015877427322301685,
+        "pitch": 0.07311506043257232,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.09801774387414852,
+          "pitch": 0.31194030586136456,
+          "title": "king size bed&nbsp;",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.3978590603951062,
+          "pitch": -0.0010968371314845626,
+          "title": "mirror",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9737538808894826,
+          "pitch": -0.3546141599548136,
+          "title": "wardrobe with concealed handle",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9306573645398277,
+          "pitch": -0.8645906775783203,
+          "title": "cnc cutting design",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.8709282803721816,
+          "pitch": -0.3949992952174277,
+          "title": "profile light&nbsp;",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.735334899967661,
+          "pitch": 0.5181696890501915,
+          "title": "fluted mdf panel with colour&nbsp; or fluted laminate as per selection&nbsp;",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "master bedroom ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
